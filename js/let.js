6d7b281aa98e06b7a55590b213053294a9d9cd01
sasmita12prusty <sasmita123prusty@gmail.com>
2021-12-30 21:19:00 +0530

    My second commit

---
+++ js/let.js
@@ -0,0 +1,8 @@
+function let(){
+    var buffer = new ArrayBuffer(80);
+    vartyped_array = new Float64Array(buffer);
+    typed_array[4] = 11;
+    console.write(typed_array.length);
+console.write(typed_array[4]);
+
+}
---
+++ js/style.js
@@ -0,0 +1,7 @@
+function hello(){
+    let a = 20;
+    let b = 10;
+    let c = "JavaScript";
+    letstr = `My age is ${a+b} and I love ${c}`;
+    document.write(letstr);
+}
